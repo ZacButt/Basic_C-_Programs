2e3616efac5cd3af1d646aefb67106e0a548348b
ZacButt <zacbutt8787@gmail.com>
2023-06-16 10:30:51 -0700

    Start of all my C# Projects using visual studio

---
+++ AcmeStudiosProject/AcmeStudiosProject/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+
+    internal class Program
+    {
+        static void Main()
+        {
+        Console.WriteLine("Welcome to Acme Accounting Systems");
+        Console.WriteLine("Remember, we're \"acounting\" on you!");
+
+        // Keep the window open
+        Console.ReadLine();
+        }
+    }
+
