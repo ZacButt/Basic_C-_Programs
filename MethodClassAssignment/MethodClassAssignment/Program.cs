cd3ebf946b540ee83edcfa95ad38fa80a93fb8c0
ZacButt <zacbutt8787@gmail.com>
2023-07-08 17:42:32 -0700

    Method Class submission

---
+++ MethodClassAssignment/MethodClassAssignment/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace ConsoleApp
+{
+    public class MathHelper
+    {
+        public void PerformMathOperation(int number1, int number2)
+        {
+            // Example math operation: subtracting number1 from number2
+            int result = number2 - number1;
+            Console.WriteLine($"The result is: {result}");
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MathHelper mathHelper = new MathHelper();
+
+            // Call the method, passing in two numbers
+            mathHelper.PerformMathOperation(5, 3);
+
+            // Call the method, specifying the parameters by name
+            mathHelper.PerformMathOperation(number1: 10, number2: 7);
+
+            Console.WriteLine("Press any key to exit...");
+            Console.ReadKey();
+        }
+    }
+}
