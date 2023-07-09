02cf3ddd15373da2ad2690aa06658409d82389c0
ZacButt <zacbutt8787@gmail.com>
2023-07-09 11:12:45 -0700

    Sample Student Assignment

---
+++ Methods&Objects/Methods&Objects/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace ConsoleApp
+{
+    // Define a class called Person
+    class Person
+    {
+        // Define properties for FirstName and LastName
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        // Define a method called SayName
+        public void SayName()
+        {
+            // Write the person's full name to the console
+            Console.WriteLine($"Name: {FirstName} {LastName}");
+        }
+    }
+
+    // Define a class called Employee that inherits from Person
+    class Employee : Person
+    {
+        // Add a property called Id of type int
+        public int Id { get; set; }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Instantiate and initialize an Employee object
+            Employee employee = new Employee()
+            {
+                FirstName = "Sample",
+                LastName = "Student",
+                Id = 123
+            };
+
+            // Call the superclass method SayName on the Employee object
+            employee.SayName();
+
+            // Wait for user input before closing the console window
+            Console.WriteLine("Press any key to exit...");
+            Console.ReadKey();
+        }
+    }
+}
