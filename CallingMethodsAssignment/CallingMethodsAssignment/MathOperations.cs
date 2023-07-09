e2eeebb3e02575e96b1f3d47aa22b7c11cc70186
ZacButt <zacbutt8787@gmail.com>
2023-07-08 17:14:27 -0700

    Calling Methods Assignment
    Still not working, but I accidentally forgot to upload this part

---
+++ CallingMethodsAssignment/CallingMethodsAssignment/MathOperations.cs
@@ -0,0 +1,20 @@
+﻿public class MathOperations
+{
+    public int AddOne(int number)
+    {
+        // Add 1 to the input number
+        return number + 1;
+    }
+
+    public int MultiplyByTwo(int number)
+    {
+        // Multiply the input number by 2
+        return number * 2;
+    }
+
+    public int Square(int number)
+    {
+        // Square the input number
+        return number * number;
+    }
+}
