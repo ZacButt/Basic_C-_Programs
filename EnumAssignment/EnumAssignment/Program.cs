1655e6f43d4f7c3793d3788cfbd91d7ab8a19070
ZacButt <zacbutt8787@gmail.com>
2023-07-10 11:39:24 -0700

    Enum days of the week assignment

---
+++ EnumAssignment/EnumAssignment/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace ConsoleApp
+{
+    class Program
+    {
+        // Enum for the days of the week
+        enum DayOfWeek
+        {
+            Sunday,
+            Monday,
+            Tuesday,
+            Wednesday,
+            Thursday,
+            Friday,
+            Saturday
+        }
+
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Enter the current day of the week:");
+                string userInput = Console.ReadLine();
+
+                // Convert the user input to enum type
+                DayOfWeek currentDay;
+                Enum.TryParse(userInput, out currentDay);
+
+                // Check if the input is a valid day of the week
+                if (Enum.IsDefined(typeof(DayOfWeek), currentDay))
+                {
+                    Console.WriteLine("Valid day of the week entered!");
+                }
+                else
+                {
+                    Console.WriteLine("Please enter an actual day of the week.");
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error: " + ex.Message);
+            }
+
+            // Wait for user input before closing the console window
+            Console.WriteLine("Press any key to exit...");
+            Console.ReadKey();
+        }
+    }
+}
