0e3ac3c60b59fac43d61ea6a64f5d07572daccf6
ZacButt <zacbutt8787@gmail.com>
2023-07-12 08:16:18 -0700

    ConstructorAssignment

---
+++ ConstructorAssignment/ConstructorAssignment/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace ConstructorAssignment
+{
+    class Program
+    {
+        // Create a constant variable
+        const int MyConstant = 100;
+
+        static void Main(string[] args)
+        {
+            // Create a variable using the var keyword
+            var myVariable = "Hello, World!";
+
+            // Chain two constructors together
+            MyClass myObject = new MyClass();
+            Console.WriteLine(myObject.Message);
+
+            // Wait for user input before closing the console window
+            Console.WriteLine("Press any key to exit...");
+            Console.ReadKey();
+        }
+    }
+
+    // Create a class with two constructors
+    class MyClass
+    {
+        public string Message { get; }
+
+        // Constructor with no parameters
+        public MyClass()
+        {
+            Message = "Default constructor";
+        }
+
+        // Constructor with a parameter
+        public MyClass(string message)
+        {
+            Message = message;
+        }
+    }
+}
