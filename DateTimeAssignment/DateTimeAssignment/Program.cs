2f7b36bb49f75b37258f7f38090ed51fc6dd74ed
ZacButt <zacbutt8787@gmail.com>
2023-07-11 17:10:37 -0700

    Date/Time Assignment

---
+++ DateTimeAssignment/DateTimeAssignment/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace DateTimeAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                // Print the current date and time to the console
+                DateTime currentDateTime = DateTime.Now;
+                Console.WriteLine("Current date and time: " + currentDateTime);
+
+                // Ask the user for a number
+                Console.Write("Enter a number: ");
+                string input = Console.ReadLine();
+
+                // Parse the user input as an integer
+                int hours = int.Parse(input);
+
+                // Calculate the future date and time
+                DateTime futureDateTime = currentDateTime.AddHours(hours);
+                Console.WriteLine("In " + hours + " hours, the time will be: " + futureDateTime);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("An error occurred: " + ex.Message);
+            }
+
+            // Wait for user input before closing the console window
+            Console.WriteLine("Press any key to exit...");
+            Console.ReadKey();
+        }
+    }
+}
