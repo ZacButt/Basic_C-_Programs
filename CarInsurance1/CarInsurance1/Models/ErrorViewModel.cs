3c6a2d136e667d257aded9b8b56f9014eab17186
ZacButt <zacbutt8787@gmail.com>
2023-07-17 12:04:02 -0700

    Car Insurance Database

---
+++ CarInsurance1/CarInsurance1/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace CarInsurance1.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
