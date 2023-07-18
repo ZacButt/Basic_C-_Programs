503acfe30716bf18e282df3515c3d987521b74fc
ZacButt <zacbutt8787@gmail.com>
2023-07-18 09:09:11 -0700

    Final Challenge

---
+++ Final_Challenge/Final_Challenge/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        using (var context = new StudentContext())
+        {
+            var student = new Student { Name = "John Doe", Age = 20 };
+            context.Students.Add(student);
+            context.SaveChanges();
+        }
+
+        Console.WriteLine("Student added to the database.");
+        Console.ReadLine();
+    }
+}
---
+++ Final_Challenge/Final_Challenge/StudentContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Runtime.Remoting.Contexts;
+
+public class StudentContext : DbContext
+{
+    public DbSet<Student> Students { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlServer("Server=localhost;Database=StudentDatabase;Trusted_Connection=True;\");
+    }
+}
