a60733feef19f4b2e1cee0096b5c77bc7c49e42b
ZacButt <zacbutt8787@gmail.com>
2023-07-21 13:04:34 -0700

    WIth the help of Wes, I got the database up and running. Thanks wes!

---
+++ FinalChallenge2/FinalChallenge2/StudentContext.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using System.Data.Entity;
+
+    public class SchoolContext : DbContext
+{
+    public SchoolContext() : base()
+    {
+
+    }
+
+    public DbSet<Student> Students { get; set; }
+    //public DbSet<grade> Grades { get; set; }
+}
